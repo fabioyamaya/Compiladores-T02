911ddd23a9bcce4b8f362a8bf5b6214f0226064f
yukio <fabio.yamaya@gmail.com>
2021-10-26 18:53:06 -0300

    Ufa, deu certo

--- src/ast/CompositeExpr.java
+++ src/ast/CompositeExpr.java
@@ -16,10 +16,78 @@ public class CompositeExpr extends Expr {
 	@Override
 	public void genC(PW pw) {
 		if (oper == Symbol.CONCAT) {
-			pw.print("(\"", false);
-			left.genC(pw);
-			right.genC(pw);
-			pw.print("\")", false);
+
+			if (left.getType() == Type.stringType) {
+				if (right.getType() == Type.integerType) {
+					pw.print("concatSI(", false);
+					left.genC(pw);
+					pw.print(", ", false);
+					right.genC(pw);
+					pw.print(", 1", false);
+					pw.print(")", false);
+				} else if (right.getType() == Type.booleanType) {
+					pw.print("concatSB(", false);
+					left.genC(pw);
+					pw.print(", ", false);
+					right.genC(pw);
+					pw.print(", 1", false);
+					pw.print(")", false);
+				} else {
+					pw.print("concatSS(", false);
+					left.genC(pw);
+					pw.print(", ", false);
+					right.genC(pw);
+					pw.print(", 1", false);
+					pw.print(")", false);
+				}
+			} else if (left.getType() == Type.booleanType) {
+				if (right.getType() == Type.integerType) {
+					pw.print("concatBI(", false);
+					left.genC(pw);
+					pw.print(", ", false);
+					right.genC(pw);
+					pw.print(", 1", false);
+					pw.print(")", false);
+				} else if (right.getType() == Type.booleanType) {
+					pw.print("concatBB(", false);
+					left.genC(pw);
+					pw.print(", ", false);
+					right.genC(pw);
+					pw.print(", 1", false);
+					pw.print(")", false);
+				} else {
+					pw.print("concatSB(", false);
+					right.genC(pw);
+					pw.print(", ", false);
+					left.genC(pw);
+					pw.print(", 0", false);
+					pw.print(")", false);
+				}
+			} else {
+				if (right.getType() == Type.integerType) {
+					pw.print("concatII(", false);
+					left.genC(pw);
+					pw.print(", ", false);
+					right.genC(pw);
+					pw.print(", 1", false);
+					pw.print(")", false);
+				} else if (right.getType() == Type.booleanType) {
+					pw.print("concatBI(", false);
+					right.genC(pw);
+					pw.print(", ", false);
+					left.genC(pw);
+					pw.print(", 0", false);
+					pw.print(")", false);
+				} else {
+					pw.print("concatSI(", false);
+					right.genC(pw);
+					pw.print(", ", false);
+					left.genC(pw);
+					pw.print(", 0", false);
+					pw.print(")", false);
+				}
+			}
+
 		} else {
 			pw.print("(", false);
 			left.genC(pw);
@@ -32,135 +100,135 @@ public class CompositeExpr extends Expr {
 	@Override
 	public Object run(Map<String, Object> memory) {
 		switch (oper) {
-			//Operação aritmética
-			case PLUS -> {
-				Integer leftValue = (Integer) left.run(memory);
-				Integer rightValue = (Integer) right.run(memory);
-				return leftValue + rightValue;
+		// Operação aritmética
+		case PLUS -> {
+			Integer leftValue = (Integer) left.run(memory);
+			Integer rightValue = (Integer) right.run(memory);
+			return leftValue + rightValue;
+		}
+		case MINUS -> {
+			Integer leftValue = (Integer) left.run(memory);
+			Integer rightValue = (Integer) right.run(memory);
+			return leftValue - rightValue;
+		}
+		case MULT -> {
+			Integer leftValue = (Integer) left.run(memory);
+			Integer rightValue = (Integer) right.run(memory);
+			return leftValue * rightValue;
+		}
+		case DIV -> {
+			Integer leftValue = (Integer) left.run(memory);
+			Integer rightValue = (Integer) right.run(memory);
+			return leftValue / rightValue;
+		}
+		case REMAINDER -> {
+			Integer leftValue = (Integer) left.run(memory);
+			Integer rightValue = (Integer) right.run(memory);
+			return leftValue % rightValue;
+		}
+		// Operação booleana
+		case OR -> {
+			Boolean leftValue = (Boolean) left.run(memory);
+			Boolean rightValue = (Boolean) right.run(memory);
+			return leftValue || rightValue;
+		}
+		case AND -> {
+			Boolean leftValue = (Boolean) left.run(memory);
+			Boolean rightValue = (Boolean) right.run(memory);
+			return leftValue && rightValue;
+		}
+		// Operação comum
+		case EQ -> {
+			return left.run(memory).equals(right.run(memory));
+		}
+		case GE -> {
+			Object leftValue = left.run(memory);
+			Object rightValue = right.run(memory);
+			if (leftValue.equals(rightValue)) {
+				return true;
 			}
-			case MINUS -> {
-				Integer leftValue = (Integer) left.run(memory);
-				Integer rightValue = (Integer) right.run(memory);
-				return leftValue - rightValue;
+			if (left.getType() == Type.booleanType) {
+				// false < true
+				return (Boolean) leftValue;
 			}
-			case MULT -> {
-				Integer leftValue = (Integer) left.run(memory);
-				Integer rightValue = (Integer) right.run(memory);
-				return leftValue * rightValue;
+			if (left.getType() == Type.stringType) {
+				String leftString = (String) leftValue;
+				String rightString = (String) rightValue;
+				return leftString.compareTo(rightString) > 0;
 			}
-			case DIV -> {
-				Integer leftValue = (Integer) left.run(memory);
-				Integer rightValue = (Integer) right.run(memory);
-				return leftValue / rightValue;
+			Integer leftInt = (Integer) leftValue;
+			Integer rightInt = (Integer) rightValue;
+			return leftInt > rightInt;
+		}
+		case GT -> {
+			Object leftValue = left.run(memory);
+			Object rightValue = right.run(memory);
+			if (leftValue.equals(rightValue)) {
+				return false;
 			}
-			case REMAINDER -> {
-				Integer leftValue = (Integer) left.run(memory);
-				Integer rightValue = (Integer) right.run(memory);
-				return leftValue % rightValue;
+			if (left.getType() == Type.booleanType) {
+				// false < true
+				return (Boolean) leftValue;
 			}
-			//Operação booleana
-			case OR -> {
-				Boolean leftValue = (Boolean) left.run(memory);
-				Boolean rightValue = (Boolean) right.run(memory);
-				return leftValue || rightValue;
+			if (left.getType() == Type.stringType) {
+				String leftString = (String) leftValue;
+				String rightString = (String) rightValue;
+				return leftString.compareTo(rightString) > 0;
 			}
-			case AND -> {
-				Boolean leftValue = (Boolean) left.run(memory);
-				Boolean rightValue = (Boolean) right.run(memory);
-				return leftValue && rightValue;
+			Integer leftInt = (Integer) leftValue;
+			Integer rightInt = (Integer) rightValue;
+			return leftInt > rightInt;
+		}
+		case LE -> {
+			Object leftValue = left.run(memory);
+			Object rightValue = right.run(memory);
+			if (leftValue.equals(rightValue)) {
+				return true;
 			}
-			//Operação comum
-			case EQ -> {
-				return left.run(memory).equals(right.run(memory));
+			if (left.getType() == Type.booleanType) {
+				// false < true
+				return (Boolean) rightValue;
 			}
-			case GE -> {
-				Object leftValue = left.run(memory);
-				Object rightValue = right.run(memory);
-				if (leftValue.equals(rightValue)) {
-					return true;
-				}
-				if (left.getType() == Type.booleanType) {
-					//false < true
-					return (Boolean) leftValue;
-				}
-				if (left.getType() == Type.stringType) {
-					String leftString = (String) leftValue;
-					String rightString = (String) rightValue;
-					return leftString.compareTo(rightString) > 0;
-				}
-				Integer leftInt = (Integer) leftValue;
-				Integer rightInt = (Integer) rightValue;
-				return leftInt > rightInt;
+			if (left.getType() == Type.stringType) {
+				String leftString = (String) leftValue;
+				String rightString = (String) rightValue;
+				return leftString.compareTo(rightString) < 0;
 			}
-			case GT -> {
-				Object leftValue = left.run(memory);
-				Object rightValue = right.run(memory);
-				if (leftValue.equals(rightValue)) {
-					return false;
-				}
-				if (left.getType() == Type.booleanType) {
-					//false < true
-					return (Boolean) leftValue;
-				}
-				if (left.getType() == Type.stringType) {
-					String leftString = (String) leftValue;
-					String rightString = (String) rightValue;
-					return leftString.compareTo(rightString) > 0;
-				}
-				Integer leftInt = (Integer) leftValue;
-				Integer rightInt = (Integer) rightValue;
-				return leftInt > rightInt;
+			Integer leftInt = (Integer) leftValue;
+			Integer rightInt = (Integer) rightValue;
+			return leftInt < rightInt;
+		}
+		case LT -> {
+			Object leftValue = left.run(memory);
+			Object rightValue = right.run(memory);
+			if (leftValue.equals(rightValue)) {
+				return false;
 			}
-			case LE -> {
-				Object leftValue = left.run(memory);
-				Object rightValue = right.run(memory);
-				if (leftValue.equals(rightValue)) {
-					return true;
-				}
-				if (left.getType() == Type.booleanType) {
-					//false < true
-					return (Boolean) rightValue;
-				}
-				if (left.getType() == Type.stringType) {
-					String leftString = (String) leftValue;
-					String rightString = (String) rightValue;
-					return leftString.compareTo(rightString) < 0;
-				}
-				Integer leftInt = (Integer) leftValue;
-				Integer rightInt = (Integer) rightValue;
-				return leftInt < rightInt;
+			if (left.getType() == Type.booleanType) {
+				// false < true
+				return (Boolean) rightValue;
 			}
-			case LT -> {
-				Object leftValue = left.run(memory);
-				Object rightValue = right.run(memory);
-				if (leftValue.equals(rightValue)) {
-					return false;
-				}
-				if (left.getType() == Type.booleanType) {
-					//false < true
-					return (Boolean) rightValue;
-				}
-				if (left.getType() == Type.stringType) {
-					String leftString = (String) leftValue;
-					String rightString = (String) rightValue;
-					return leftString.compareTo(rightString) < 0;
-				}
-				Integer leftInt = (Integer) leftValue;
-				Integer rightInt = (Integer) rightValue;
-				return leftInt < rightInt;
+			if (left.getType() == Type.stringType) {
+				String leftString = (String) leftValue;
+				String rightString = (String) rightValue;
+				return leftString.compareTo(rightString) < 0;
 			}
-			case NEQ -> {
-				return !left.run(memory).equals(right.run(memory));
-			}
-			case CONCAT -> {
-				Object leftValue = left.run(memory);
-				Object rightValue = right.run(memory);
-				return leftValue.toString() + rightValue.toString();
-			}
-			default -> {
-				System.out.println("Operador " + oper.toString() + " não implementado");
-				return 0;
-			}
+			Integer leftInt = (Integer) leftValue;
+			Integer rightInt = (Integer) rightValue;
+			return leftInt < rightInt;
+		}
+		case NEQ -> {
+			return !left.run(memory).equals(right.run(memory));
+		}
+		case CONCAT -> {
+			Object leftValue = left.run(memory);
+			Object rightValue = right.run(memory);
+			return leftValue.toString() + rightValue.toString();
+		}
+		default -> {
+			System.out.println("Operador " + oper.toString() + " não implementado");
+			return 0;
+		}
 		}
 
 	}
@@ -171,7 +239,7 @@ public class CompositeExpr extends Expr {
 		if (oper == Symbol.EQ || oper == Symbol.NEQ || oper == Symbol.LE || oper == Symbol.LT || oper == Symbol.GE
 				|| oper == Symbol.GT || oper == Symbol.AND || oper == Symbol.OR)
 			return Type.booleanType;
-		else if (oper == Symbol.CONCAT) 
+		else if (oper == Symbol.CONCAT)
 			return Type.stringType;
 		else
 			return Type.integerType;
--- src/ast/Program.java
+++ src/ast/Program.java
@@ -21,48 +21,72 @@ public class Program {
 		pw.println("#include <string.h>", true);
 		pw.println("", false);
 
-		//Funcao de alocacao de memoria para strings
-		pw.println("// Realiza alocação de memória para strings\r\n"
-				+ "void reallocStr(char **strPtr, int size){\r\n"
-				+ "    char *tmp = NULL;\r\n"
-				+ "\r\n"
-				+ "    tmp = realloc(*strPtr, size + 1);\r\n"
-				+ "\r\n"
-				+ "    if (!tmp) {\r\n"
-				+ "        free(*strPtr);\r\n"
-				+ "        printf(\"Erro na alocacao de memoria\");\r\n"
-				+ "        exit(0);\r\n"
-				+ "    }\r\n"
-				+ "    *strPtr = tmp;\r\n"
-				+ "}", true);
-		pw.println("", false);
+		pw.println("// Funções ++\n"
+				+ "int countDigitos(int n) {\r\n" + "    int count = 0;\r\n" + "    while (n != 0) {\r\n"
+				+ "        n = n / 10;\r\n" + "        count++;\r\n" + "    }\r\n" + "    return count;\r\n" + "}\r\n"
+				+ "\r\n" + "char* concatSI(char* string, int integer, int ordem) {\r\n"
+				+ "    char* stringFinal = malloc(strlen(string) + countDigitos(integer) + 1);\r\n"
+				+ "    if (ordem) {\r\n" + "        sprintf(stringFinal, \"%s%d\", string, integer);\r\n"
+				+ "    } else {\r\n" + "        sprintf(stringFinal, \"%d%s\", integer, string);\r\n" + "    }\r\n"
+				+ "    return stringFinal;\r\n" + "}\r\n" + "\r\n"
+				+ "char* concatSB(char* string, int boolean, int ordem) {\r\n"
+				+ "    char* stringFinal = malloc(strlen(string) + (boolean ? 4 : 5) + 1);\r\n" + "    if (ordem) {\r\n"
+				+ "        sprintf(stringFinal, \"%s%s\", string, boolean ? \"true\" : \"false\");\r\n"
+				+ "    } else {\r\n"
+				+ "        sprintf(stringFinal, \"%s%s\", boolean ? \"true\" : \"false\", string);\r\n" + "    }\r\n"
+				+ "    return stringFinal;\r\n" + "}\r\n" + "\r\n"
+				+ "char* concatSS(char* string1, char* string2, int ordem) {\r\n"
+				+ "    char* stringFinal = malloc(strlen(string1) + strlen(string2) + 1);\r\n" + "    if (ordem) {\r\n"
+				+ "        sprintf(stringFinal, \"%s%s\", string1, string2);\r\n" + "    } else {\r\n"
+				+ "        sprintf(stringFinal, \"%s%s\", string2, string1);\r\n" + "    }\r\n"
+				+ "    return stringFinal;\r\n" + "}\r\n" + "\r\n"
+				+ "char* concatBI(int boolean, int integer, int ordem) {\r\n"
+				+ "    char* stringFinal = malloc((boolean ? 4 : 5) + countDigitos(integer) + 1);\r\n"
+				+ "    if (ordem) {\r\n"
+				+ "        sprintf(stringFinal, \"%s%d\", boolean ? \"true\" : \"false\", integer);\r\n"
+				+ "    } else {\r\n"
+				+ "        sprintf(stringFinal, \"%d%s\", integer, boolean ? \"true\" : \"false\");\r\n" + "    }\r\n"
+				+ "    return stringFinal;\r\n" + "}\r\n" + "\r\n"
+				+ "char* concatBB(int boolean1, int boolean2, int ordem) {\r\n"
+				+ "    char* stringFinal = malloc((boolean1 ? 4 : 5) + (boolean2 ? 4 : 5) + 1);\r\n"
+				+ "    if (ordem) {\r\n" + "        sprintf(stringFinal, \"%s%s\", boolean1 ? \"true\" : \"false\",\r\n"
+				+ "                boolean2 ? \"true\" : \"false\");\r\n" + "    } else {\r\n"
+				+ "        sprintf(stringFinal, \"%s%s\", boolean2 ? \"true\" : \"false\",\r\n"
+				+ "                boolean1 ? \"true\" : \"false\");\r\n" + "    }\r\n" + "    return stringFinal;\r\n"
+				+ "}\r\n" + "\r\n" + "char* concatII(int integer1, int integer2, int ordem) {\r\n"
+				+ "    char* stringFinal =\r\n"
+				+ "        malloc(countDigitos(integer1) + countDigitos(integer2) + 1);\r\n" + "    if (ordem) {\r\n"
+				+ "        sprintf(stringFinal, \"%d%d\", integer1, integer2);\r\n" + "    } else {\r\n"
+				+ "        sprintf(stringFinal, \"%d%d\", integer2, integer1);\r\n" + "    }\r\n"
+				+ "    return stringFinal;\r\n" + "}", true);
 		
-		//FunÃ§Ã£o printbool
-		pw.println("// Imprime booleanos com valores true ou false\r\n" +
-				"void printBool(int newline, int bool) {\n" +
-				"    if(bool) {\n" +
-				"        printf(\"true\");  \n" +
-				"    } else {\n" +
-				"        printf(\"false\");\n" +
-				"    }\n" +
-				"        \n" +
-				"    if(newline) {\n" +
-				"        printf(\"\\n\")\n" +
-				"    }\n" +
-				"}", true);
+		pw.println("", false);
+
+		// Funcao de alocacao de memoria para strings
+		pw.println("// Realiza alocação de memória para strings\r\n" + "void reallocStr(char **strPtr, int size){\r\n"
+				+ "    char *tmp = NULL;\r\n" + "\r\n" + "    tmp = realloc(*strPtr, size + 1);\r\n" + "\r\n"
+				+ "    if (!tmp) {\r\n" + "        free(*strPtr);\r\n"
+				+ "        printf(\"Erro na alocacao de memoria\");\r\n" + "        exit(0);\r\n" + "    }\r\n"
+				+ "    *strPtr = tmp;\r\n" + "}", true);
+		pw.println("", false);
+
+		// FunÃ§Ã£o printbool
+		pw.println("// Imprime booleanos com valores true ou false\r\n" + "void printBool(int newline, int bool) {\n"
+				+ "    if(bool) {\n" + "        printf(\"true\");  \n" + "    } else {\n"
+				+ "        printf(\"false\");\n" + "    }\n" + "        \n" + "    if(newline) {\n"
+				+ "        printf(\"\\n\");\n" + "    }\n" + "}", true);
 		pw.println("", false);
 
 		pw.println("void main() {", true);
-		pw.println("", false);
-		
+
 		pw.add();
 
-		pw.println("",true);
+		pw.println("", true);
 		statementList.genC(pw);
-		
+
 		// Libera memoria alocada
 		statementList.genCFreeStr(pw);
-		
+
 		pw.sub();
 		pw.println("}", true);
 	}
